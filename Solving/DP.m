3e9aa9f2b33033c5decaada783d4bad596aa02bf
Discount <33273755+Dis-count@users.noreply.github.com>
2019-10-14 16:48:53 +0800

    还差一步

--- Solving/DP.m
+++ Solving/DP.m
@@ -1,34 +1,62 @@
-function [result, s] = DP(v,t,beta,z)
+function [res, s] = DP(v,t,beta,z)
 
 % t and beta are vectors
 % z = 9.5
 % t =[5 4 3 2];
 % beta = [14.5 8 12.5 4]
-% 注意这里不能有 V (u \neq v)
+% 注意这里不能存在 V (u \neq v)
 
-u = 1; % counting
+% u = v-1; % counting
 
-s = zeros(1,v); % 记录集合中player
+s = zeros(1,v); % 记录集合��? player
+
+ss = zeros(v-1,v);
+
+
+res = zeros(v-1,1); % record the min value
+
+for uu = v-1: -1 : 1
 
 result = z;
 
-for i =1 : v
+  for i = v: -1 : 1
 
-    if (u*t(i) < beta(i))&(u < v)
+    ii = uu;
 
-      result = result + u*t(i)- beta(i);
+    if ii*t(i) < beta(i)
 
-      u = u + 1;
+      result = result + ii*t(i) - beta(i);
 
       s(i) = 1;  % add the player
 
-    else
-
-      s(i) = 0;
+      ii = ii-1;  % problem
 
     end
 
+    if ii < 1
+
+      break
+
+    end
+
+    if (i-1) == ii
+
+      s(1:ii) = 1;
+
+      result =  dot((1:ii),t(1:ii)) - sum(beta(1:ii));
+
+      break
+
+    end
+
+  end
+
+  res(uu) = result;
+
+  ss(uu,:) = s;
+
 end
 
+ss
 
 end
