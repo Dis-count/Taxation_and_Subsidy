5be1f2e2e7b913452fb5d951b427001e3bd5029f
Discount <492193947@qq.com>
2021-03-28 23:23:58 +0800

    update

--- Solving/IPCtest.m
+++ Solving/IPCtest.m
@@ -8,15 +8,15 @@ function [Pstar,omega] = IPCtest()
 % t = [5;4;3;2];
 
 % t= [9;8;7;5.5;4];
-t = [7.5,6,5.5,4,3,1,1.5,1.5];
+t = [7.5,6,5.5,4,3,1.5,1.5,1.5];
 
 v = length(t);
-Pstar = [60,68]; % The set of breakpoints
+Pstar = [41,68]; % The set of breakpoints
 % Pstar = interval;
 
 % Pbig = [11,16];
 
-Pbig = [60,68];  % The interval of price and will gradually decrease
+Pbig = [50,68];  % The interval of price and will gradually decrease
 
 % Pbig  = interval;
 
@@ -27,7 +27,7 @@ count = 0
 while ~isempty(Pbig)
   count = count + 1
 
-  if count > 50
+  if count > 40
     disp('There is something wrong in the loop.')
     break
   end
@@ -40,7 +40,7 @@ while ~isempty(Pbig)
 
   omega(2) = a2;
 
-  slope =  (a2-a1)/(Pbig(1,2)-Pbig(1,1));
+  slope =  (a2-a1)/(Pbig(1,2)-Pbig(1,1)); % negative
 
   if (round(c2,5) == round(slope,5))||(abs(b1-slope)<1e-5)
 
@@ -52,26 +52,38 @@ while ~isempty(Pbig)
 
     zinter = (b1*Pbig(1,1) - Pbig(1,2)*c2 + a2 - a1)/(b1-c2);
 
-    if zinter < 41||zinter > 68
+    % if zinter < 41||zinter > 68
+    %
+    %     disp('There is something wrong')
+    %
+    %     break
+    %
+    % end
 
-        disp('There is something wrong')
+    % omega1 = (zinter - Pbig(1,2))*b2 + a2;
+    omega1 = (zinter - Pbig(1,2))*c2 + a2;
 
-        break
+    [a,b,c] = CP(v,t,zinter);
 
+    if abs(omega1-a)<1e-5 % the subsidy equals, this is the breakpoint, then delete this interval, store the breakpoint
+
+      omega = [omega,omega1];
+
+      Pstar = [Pstar,zinter];
+
+      Pbig(1,:) = [];
+
+    else
+
+      Pbig(end+1,:) = [Pbig(1,1),zinter];
+      % Notice that there is already add a new row
+
+      Pbig(end+1,:) = [zinter,Pbig(1,2)];
+
+      Pbig(1,:) = [];
     end
 
-    omega1 = (zinter - Pbig(1,2))*b2 + a2;
-
-    omega = [omega,omega1];
-
-    Pstar = [Pstar,zinter];
-
-    Pbig(end+1,:) = [Pbig(1,1),zinter];
-    % Notice that there is already add a new row
-
-    Pbig(end+1,:) = [zinter,Pbig(1,2)];
-
-    Pbig(1,:) = [];
+    % Pbig(1,:) = [];
 
     Pstar = sort(Pstar);
 
