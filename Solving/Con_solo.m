80acd0aa029e32d809c6b91ce3334bef48a0ddca
Discount <33273755+Dis-count@users.noreply.github.com>
2021-04-03 12:38:01 +0800

    update

--- Solving/Con_solo.m
+++ Solving/Con_solo.m
@@ -1,7 +1,6 @@
-function Construct()
+function Con_solo(i)
 
-%  This function is used to construct the price-subsidy function.
-
+%  This function is used to construct one(i th) interval of the price-subsidy function.
 % t = [7.5,6,  5.5,4,3,1.5,1.5,1.5];
 t = [7,  6.5,6.5,4,4,3.5,2.5,2, 1.5,1];
 % t = rand(1,n)
@@ -11,21 +10,22 @@ x = []; % Restore the value of price at the breakpoints.
 
 y = []; % Restore the value of subsidy at the breakpoints.
 
-for i = 1: length(interval)-1
-  aa = interval(i,1:2)
-  [list_x, list_y] = IPCtest(t,[aa(1),aa(2)-0.5]);
-  x = [x,list_x];
-  y = [y,list_y];
-end
-
-x = [x,0,interval(1,2)];
-y = [y,0,0];
+aa = interval(i,1:2);
+[list_x, list_y] = IPCtest(t,[aa(1),aa(2)-0.5]);
+x = [x,list_x];
+y = [y,list_y];
 
 point_xy = [x;y];
 point_xy = sortrows(point_xy',1)';
 
 plot(point_xy(1,:),point_xy(2,:));
 
-saveas(gcf,'IPC.pdf');
+xlabel('¶¨¼Û')
+
+ylabel('²¹Ìù')
+
+legend('²¹Ìù','Location','NorthEast' );
+
+saveas(gcf,'soloIPC1.pdf');
 
 end
